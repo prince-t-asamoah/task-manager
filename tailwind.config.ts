a707c44e1cfe40637a1502dcf4cadd1d97354a11
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-03 09:50:34 +0000

    add custom font family and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,12 @@ const config: Config = {
     ],
     theme: {
         extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic':
-                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            fontFamily: {
+                sans: ['Open sans', 'sans-serif'],
+            },
+            colors: {
+                'lepton-lilac': '#9c7481',
+                'boson-blue': '#141530',
             },
         },
     },
