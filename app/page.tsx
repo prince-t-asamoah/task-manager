0dee17621c1d2389b0418c6956ee76a04d87cada
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-03 09:51:00 +0000

    add page header and app title

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
     return (
-        <main>
-            <h1>Task Manager</h1>
-        </main>
+        <div className="App">
+            <header className="bg-boson-blue pt-8 px-5 lg:px-50 lg:pb-22">
+                <h1 className="text-sm lg:text-lg font-bold text-gray-100 text-center pt-8 pb-5 tracking-[12px] lg:tracking-[20px] uppercase lg:py-10">
+                    Task Manager
+                </h1>
+            </header>
+            <main></main>
+        </div>
     );
 }
