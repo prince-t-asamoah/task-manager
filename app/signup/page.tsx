7dede7e4bc5b5f1cf9520e095bdd262b7b1a5430
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-03 17:16:14 +0000

    update form layout for medium size devices

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -13,8 +13,8 @@ export default function SignUp() {
     return (
         <div className="min-h-screen bg-gray-100 flex">
             <div className="lg:block flex-1 hidden bg-boson-blue"></div>
-            <div className="flex-1  flex flex-col gap-4 lg:gap-8 items-center">
-                <div className="p-5 lg:px-10 lg:pt-10 w-full">
+            <div className="flex-1 flex flex-col lg:gap-8 items-center relative">
+                <div className="px-5 py-8 md:p-8 lg:px-10 lg:pt-10 w-full">
                     <Link
                         href="/"
                         className="font-semibold text-boson-blue lg:hover:underline inline-flex items-center gap-2"
@@ -23,8 +23,8 @@ export default function SignUp() {
                         <span>Back</span>
                     </Link>
                 </div>
-                <div className="py-4 w-full flex justify-center">
-                    <div className="w-4/5 lg:w-3/5">
+                <div className="flex-grow py-4 w-full flex justify-center items-center">
+                    <div className="w-4/5 md:w-3/5">
                         <header className="text-center pb-5 lg:pt-3 lg:pb-8">
                             <h1 className="text-gray-950 text-xl lg:text-3xl font-bold">
                                 Create your account
@@ -35,7 +35,7 @@ export default function SignUp() {
                         </header>
                         <form>
                             <div className="flex flex-col gap-5 lg:gap-8">
-                                <div className="flex flex-col gap-4">
+                                <div className="flex flex-col gap-4 md:py-5">
                                     <Input
                                         type="text"
                                         label="First Name"
