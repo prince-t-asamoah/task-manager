313d79a0a34bca211ea480cf9db8b7b6e7571b49
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-03 12:19:33 +0000

    add task list item

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { CgMathPlus } from 'react-icons/cg';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { LuListTodo } from 'react-icons/lu';
 
 export default function Home() {
     return (
@@ -23,7 +24,7 @@ export default function Home() {
                 </div>
             </header>
             <main className="py-12 px-5 lg:px-40 flex-grow bg-gray-100">
-                <div className=" flex justify-between items-center">
+                <div className="pb-4 flex justify-between items-center">
                     <h2 className="text-lepton-lilac">Latest</h2>
                     <Link
                         href="/new"
@@ -32,6 +33,42 @@ export default function Home() {
                         <CgMathPlus size={20} />
                         Add task
                     </Link>
+                </div>
+                <div className="w-full py-2">
+                    <ul className="flex flex-col gap-5">
+                        <li className="h-[130px] py-3 px-4 bg-white border-b shadow-md flex flex-col justify-between rounded-md">
+                            <div className="text-sm flex flex-col gap-1">
+                                <h3 className=" text-gray-950 font-semibold">
+                                    My first task
+                                </h3>
+                                <p className="text-xs text-gray-500 line-clamp-2">
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit, sed do eiusmod tempor
+                                    incididunt ut labore et dolore magna aliqua.
+                                    Ut enim ad minim veniam, quis nostrud
+                                    exercitation ullamco laboris nisi ut aliquip
+                                    ex ea commodo consequat. Duis aute irure
+                                    dolor in reprehenderit in voluptate velit
+                                    esse cillum dolore eu fugiat nulla pariatur.
+                                </p>
+                            </div>
+                            <div className="text-xs flex flex-col lg:flex-row gap-2">
+                                <span className="w-fit text-gray-600 bg-blue-200 rounded-full px-2 py-0.5">
+                                    groceries
+                                </span>
+                                <div className="flex-grow flex justify-between">
+                                    <span className="flex items-center gap-1.5">
+                                        <LuListTodo
+                                            size={16}
+                                            class="text-gray-600"
+                                        />
+                                        <span>0/5 completed</span>
+                                    </span>
+                                    <span>3 days left</span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </main>
             <footer className="w-full h-fit px-4 py-2 flex justify-center absolute bottom-0 border-t lg:hidden">
