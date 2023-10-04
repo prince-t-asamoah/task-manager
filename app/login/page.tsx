1888c8632215247ded9a610eed7d9fd88e23c822
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-04 13:13:56 +0000

    add two div column layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,10 @@
+'use client';
+
 export default function Login() {
-    return <div>Login Page</div>;
+    return (
+        <div className="min-h-screen bg-gray-100 flex">
+            <div className="lg:block flex-1 hidden bg-boson-blue"></div>
+            <div className="flex-1 flex flex-col lg:gap-8 items-center relative"></div>
+        </div>
+    );
 }
