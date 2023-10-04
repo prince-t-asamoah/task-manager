b73cbcff8625bd634cb28a24af19e0ec66122121
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-10-04 13:15:45 +0000

    add back to homepage link

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,23 @@
 'use client';
 
+import Link from 'next/link';
+import { FaChevronLeft } from 'react-icons/fa6';
+
 export default function Login() {
     return (
         <div className="min-h-screen bg-gray-100 flex">
             <div className="lg:block flex-1 hidden bg-boson-blue"></div>
-            <div className="flex-1 flex flex-col lg:gap-8 items-center relative"></div>
+            <div className="flex-1 flex flex-col lg:gap-8 items-center relative">
+                <div className="px-5 py-8 md:p-8 lg:px-10 lg:pt-10 w-full">
+                    <Link
+                        href="/"
+                        className="font-semibold text-boson-blue lg:hover:underline inline-flex items-center gap-2"
+                    >
+                        <FaChevronLeft />
+                        <span>Back</span>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
